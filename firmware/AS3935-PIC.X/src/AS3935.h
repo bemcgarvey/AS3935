cf95392335be4cb4af4401870398eda2383d2c1c
Brad McGarvey <45929861+bemcgarvey@users.noreply.github.com>
2021-04-12 13:40:45 -0600

    Initial Arduino and PIC projects

---
+++ firmware/AS3935-PIC.X/src/AS3935.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   AS3935.h
+ * Author: bemcg
+ *
+ * Created on April 12, 2021, 1:25 PM
+ */
+
+#ifndef AS3935_H
+#define	AS3935_H
+
+
+
+#endif	/* AS3935_H */
+
---
+++ firmware/AS3935-PIC.X/src/main.c
@@ -0,0 +1,18 @@
+/*
+ * File:   main.c
+ * Author: bemcg
+ *
+ * Created on April 12, 2021, 1:25 PM
+ */
+
+
+#include <xc.h>
+#include "AS3935.h"
+#include "LCD.h"
+
+void main(void) {
+    LCDInit();
+    while (1) {
+        
+    }
+}
